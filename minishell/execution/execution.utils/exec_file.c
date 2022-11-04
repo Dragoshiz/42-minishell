874509a1c005bcfb5e2fa52a0f92db2ffee93916
Dragos Imbrea <dragosimbrea@gmail.com>
2022-11-04 11:51:32 +0100

    hello

--- minishell/execution/execution.utils/exec_file.c
+++ minishell/execution/execution.utils/exec_file.c
@@ -6,23 +6,33 @@
 /*   By: dimbrea <dimbrea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 10:12:35 by dimbrea           #+#    #+#             */
-/*   Updated: 2022/11/03 10:58:28 by dimbrea          ###   ########.fr       */
+/*   Updated: 2022/11/03 13:06:18 by dimbrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	ft_exec_file(t_parsing *parsing)
+int	ft_exec_file(t_parsing *parsing)
 {
-	int		i;
+	int		pid;
 	t_token	*current;
 
+	pid = 0;
 	current = parsing->token_list->head;
-	i = 0;
 	while (current != NULL)
 	{
-		printf("%s",current->data);
+		if (access(current->data, F_OK) == 0)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				if (execve(current->data, NULL, NULL) < 0)
+					perror("");
+			}
+			waitpid(pid, NULL, 0);
+			return (1);
+		}
 		current = current->next;
 	}
+	return (0);
 }
-
